ae6e02eca5df997945c7375c01211eb6d8417abd
Henry Singer <henrysinger24@gmail.com>
2025-02-06 09:21:16 -0500

    Adding pydantic

--- analysis-api/main.py
+++ analysis-api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Dict, Any, Optional, List
 import os
 import pandas as pd
